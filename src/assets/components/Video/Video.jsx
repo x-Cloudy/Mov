c665b3d5edeeb95c53d2017adc971129fa509a24
Cloudy <cloudibr@gmail.com>
2023-12-29 11:24:49 -0300

    video back button fix

--- src/assets/components/Video/Video.jsx
+++ src/assets/components/Video/Video.jsx
@@ -3,29 +3,28 @@ import { row1, intro } from "../RowData/row1-data";
 import { RiArrowGoBackFill } from "react-icons/ri";
 import logo from '../Header/imgs/logo-2icon.png';
 import './Video.css'
-import { useEffect, useRef } from "react";
-
 
 export function Video() {
-
+  
   return (
     <div className='video-container'>
-        <img src={logo} className="button-icon-video"/>
-      <Link to={'/Mov'} className="video-button">
-        <div className="btn-back">
-          <p><RiArrowGoBackFill /></p>
-        </div>
-      </Link>
+      
+      <div className="video-back-button">
+        <Link to={'/Mov'} className="video-button">
+        <img src={logo} className="button-icon-video" />
+          <div className="btn-back">
+            <p><RiArrowGoBackFill /></p>
+          </div>
+        </Link>
+      </div>
 
-      <Outlet /> 
+      <Outlet />
     </div>
   )
 }
 
 export function VideoId() {
   const { id } = useParams();
-  const playerVid = useRef()
-
 
   let videoAction
   if (id === 'intro') {
@@ -35,6 +34,6 @@ export function VideoId() {
   }
 
   return (
-    <iframe ref={playerVid} className="video-player" src={`https://www.youtube.com/embed/${videoAction}?autoplay=1&mute=0&rel=0;si=X27ge2SJWDsc28jf;`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; gyroscope; picture-in-picture; web-share" allowFullScreen='1'></iframe>
+    <iframe className="video-player" src={`https://www.youtube.com/embed/${videoAction}?autoplay=1&mute=0&rel=0;si=X27ge2SJWDsc28jf;`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; gyroscope; picture-in-picture; web-share" allowFullScreen='1'></iframe>
   )
 }
